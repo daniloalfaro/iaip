c490917c804ba2720f61ae01e011bc5f3fa55ffe
danilo <alexander.danilo.alfaro@gmail.com>
2020-01-27 17:11:43 -0600

    ...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,10 @@ class HomeController < ApplicationController
     @institutions = Institution.paginate(page: params[:page], per_page: 50)
     @institutions2 = Institution.all
 
-    respond_to do |format|
-      format.html
-      format.csv { send_data @institutions2.to_csv, filename: "institutions-#{Date.today}.csv" }
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.csv { send_data @institutions2.to_csv, filename: "institutions-#{Date.today}.csv" }
+    # end
   end
 
 end
